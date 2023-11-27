bcb0f55726b36d103fd42d98a10877ed09e64bc6
sayaneko514 <59753147+sayaneko514@users.noreply.github.com>
2023-11-27 14:09:54 -0800

    Update index_video_objects.py
    Fix duplication in code

--- index_video_objects.py
+++ index_video_objects.py
@@ -1,4 +1,3 @@
-
 import cv2
 import numpy as np
 
@@ -14,18 +13,20 @@ with open("path/to/coco.names", "r") as f:
 video_path = "path/to/your/video/file.mp4"
 cap = cv2.VideoCapture(video_path)
 
-# List to store deteced objects with timestamps
+# List to store detected objects
 detected_objects = []
 
 while True:
     # Read a frame from the video
     ret, frame = cap.read()
-
     if not ret:
         break
 
-    # Get frame dimensions
+    # Get frame dimensions and current timestamp
     height, width, _ = frame.shape
+
+    # Get current timestamp
+    current_time = cap.get(cv2.CAP_PROP_POS_MSEC)
 
     # Create a blob from the frame
     blob = cv2.dnn.blobFromImage(frame, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
@@ -36,42 +37,37 @@ while True:
     # Run forward pass to get output from the output layers
     outs = net.forward(net.getUnconnectedOutLayersNames())
 
-    # Get current timestamp
-    current_time = cap.get(cv2.CAP_PROP_POS_MSEC)
-
+    # Process the detected objects
     for out in outs:
         for detection in out:
-            # Process the detected objects
-            for out in outs:
-                for detection in out:
-                    scores = detection[5:]
-                    class_id = np.argmax(scores)
-                    confidence = scores[class_id]
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
 
-                    if confidence > 0.5:  # Confidence threshold
-                        # Get the coordinates of the bounding box
-                        center_x = int(detection[0] * width)
-                        center_y = int(detection[1] * height)
-                        w = int(detection[2] * width)
-                        h = int(detection[3] * height)
+            if confidence > 0.5:  # Confidence threshold
+                # Coordinates of the bounding box
+                center_x = int(detection[0] * width)
+                center_y = int(detection[1] * height)
+                w = int(detection[2] * width)
+                h = int(detection[3] * height)
+                
+                # Draw a bounding box
+                x = int(center_x - w / 2)
+                y = int(center_y - h / 2)
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-                        # Draw a bounding box
-                        x = int(center_x - w / 2)
-                        y = int(center_y - h / 2)
-                        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
-                        # Display the class label and confidence
-                        label = f"{classes[class_id]}: {confidence:.2f}"
-                        cv2.putText(frame, label, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-
-                        # Add detected object and timestamp to the list
-                        detected_objects.append({
-                            "time": current_time,
-                            "class_id": class_id,
-                            "lable": classes[class_id],
-                            "confidence": confidence,
-                            "bounding_box": (x, y, w, h)
-                        })
+                # Add detected object to the list
+                detected_objects.append({
+                    "time": current_time,
+                    "class": class_id,
+                    "label": classes[class_id],
+                    "confidence": float(confidence),
+                    "bounding_box": [x, y, w, h]
+                })
+                
+                # Display the class label and confidence
+                label = f"{classes[class_id]}: {confidence:.2f}"
+                cv2.putText(frame, label, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
     # Display the resulting frame
     cv2.imshow('Object Detection', frame)
@@ -80,6 +76,6 @@ while True:
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# Release the video capture object and close the display window
+# Release video capture and close windows
 cap.release()
 cv2.destroyAllWindows()
