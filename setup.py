91aa88358767e26527e1ef2ed0d9c671a14d6db5
papitz <paul@familie-hoeppner.de>
2023-11-28 15:42:06 -0800

    Fixed module dependencies

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='short_search',
     version='0.1.0',
     py_modules=['short_search'],
+    packages=find_packages(),
     install_requires=[
         'Click',
     ],
