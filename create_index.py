041337d43e56627cfb1bcc1c3dff2299d6cd0445
sayaneko514 <59753147+sayaneko514@users.noreply.github.com>
2023-11-28 16:58:26 -0800

    Add basic indexing
    Add JSON output from get_object_from_video.py
    Mod JSON output from transcribe.py
    Add create_index.py to merge multiples JSON files generated
    Add search_index.py to read the index populated by create_index and perform searches based on different fields defined in schema

---
+++ create_index.py
@@ -0,0 +1,70 @@
+import json
+import os
+from whoosh.index import create_in
+from whoosh.fields import Schema, TEXT, ID
+
+#Define the schema
+schema = Schema(
+    video_id=ID(stored=True, unique=True),
+    transcription=TEXT(stored=True),
+    summary=TEXT(stored=True),
+    keywords=TEXT(stored=True),
+    detected_objects=TEXT(stored=True)
+)
+
+#Read data from a JSON file
+def read_json(file_path):
+    # Check if the file exists
+    if not os.path.exists(file_path):
+        print(f"File not found: {file_path}")
+        return {}
+
+    try:
+        with open(file_path, 'r') as file:
+            data = json.load(file)
+            
+        if not isinstance(data, dict):
+            print(f"Invalid format in file: {file_path}")
+            return {}
+
+        return data
+
+    except json.JSONDecodeError as e:
+        print(f"Error reading JSON file {file_path}: {e}")
+        return {}
+
+
+#Merge data from JSON files
+def merge_data(transcription_data, object_data):
+    merged_data = {}
+    for video_id, data in transcription_data.items():
+        merged_data[video_id] = {
+            "transcription": data.get("transcription", ""),
+            "summary": data.get("summary", ""),
+            "keywords": ', '.join(data.get("keywords", [])),
+            "detected_objects": ', '.join(object_data.get(video_id, []))
+        }
+    return merged_data
+
+#Create the index
+def create_index(data, index_dir):
+    if not os.path.exists(index_dir):
+        os.mkdir(index_dir)
+    ix = create_in(index_dir, schema)
+    writer = ix.writer()
+    for video_id, content in data.items():
+        writer.add_document(
+            video_id=video_id,
+            transcription=content["transcription"],
+            summary=content["summary"],
+            keywords=content["keywords"],
+            detected_objects=content["detected_objects"]
+        )
+    writer.commit()
+
+if __name__ == "__main__":
+    transcription_data = read_json("transcription_data.json")
+    object_data = read_json("detected_objects.json")
+
+    merged_data = merge_data(transcription_data, object_data)
+    create_index(merged_data, "indexdir")
